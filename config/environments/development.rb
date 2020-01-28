badac4c27653996e8487f1921c549e23c5486c81
giorgi01 <giorgi.tamarashvili.1@btu.edu.ge>
2020-01-28 19:47:29 +0400

    z

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,7 +58,6 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  config.action_cable.allowed_request_origins = ['localhost:3000']
+  config.action_cable.url = "ws://localhost:3000/cable"
 end
-
-  # config.action_cable.disable_request_forgery_protection = true
-  # config.action_cable.allowed_request_origins = ['https://message-me-giorgi.herokuapp.com']
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.web_socket_server_url = "wss://message-me-giorgi.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ['https://message-me-giorgi.herokuapp.com', 'http://message-me-giorgi.herokuapp.com']
 end
