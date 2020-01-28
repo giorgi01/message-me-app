c7911ea371f4b4e5173e2035f1a981735770d4bb
giorgi01 <giorgi.tamarashvili.1@btu.edu.ge>
2020-01-28 18:40:20 +0400

    Added UserController, Signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
-      flash[:success] = "Welcome to the alpha blog #{@user.username}"
+      flash[:success] = "Welcome to MessageMe app #{@user.username}"
       redirect_to root_path
     else
       render 'new'
